16431d0dc3f17cd97e935df8ffcc01898ca296bf
MADHAV BAHL <madhavbahl10@gmail.com>
2017-10-11 08:41:05 +0530

    Added Materialize Designs

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ ShoppingList-Desktop-App
 
 A shopping list maintainence desktop application build on ElectronJS
 
+
+![image](https://user-images.githubusercontent.com/26179770/31354680-daad71c0-ad54-11e7-864e-f837ef16ff21.png)
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
+// Set environment
+process.env.NODE_ENV = 'production';
+
 let mainWindow;
 let addWindow;
 
@@ -67,7 +70,10 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'Clear Items'
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {
                 label: 'Quit',
